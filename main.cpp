b9676ddba896fb38c31f3caca389ec14088bd995
Joao Salvado <joao.salvado@protonmail.com>
2022-04-06 19:43:32 +0200

    rh with final trajectory

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ using namespace casadi;
 #include "Plotter_dt.h"
 
 
+
+
 double estimate_distance( std::vector<double> x0, std::vector<double> xf){
     double alpha = 0.5;
     double beta = 1.0;
@@ -29,6 +31,17 @@ double estimate_T1(int  N, double L, double v_max){
     return N * L / (beta * v_max);
 }
 
+double estimate_N( double T1, double L, double v_max){
+    double beta = 1.0;
+    return beta * T1 * v_max / L;
+}
+
+int N_init = 10; // 3*T for RK4
+double L = 0.5;
+double v_std = 0.5;
+double v_max = 1.0;
+double T1 = estimate_T1(3*N_init, L, v_max);
+
 // 0.2 - State: SE2
 struct se1{
     casadi::MX X; //Discrete states
@@ -37,6 +50,7 @@ struct se1{
     casadi::SX o_ode{SX::sym("o")};
     casadi::SX X_ode() { return casadi::SX::vertcat({x_ode, y_ode, o_ode}); };
 };
+
 // 0.3 - Control: Speed and Angular velocity
 struct vw{
     casadi::MX U; // Discrete controls
@@ -46,18 +60,21 @@ struct vw{
 };
 
 void rh(std::vector<double> x0, std::vector <double> u0, std::vector<double> xf, double Tf,
-std::vector<double> &xf_new, std::vector<double> &u0_new ){
+std::vector<double> &xf_new, std::vector<double> &u0_new , bool final = false){
     // 1.1 - Params
-    int n = 3;
+    int n =  3;
 
-
-    int N = 10; //3*T for RK4
-    double L = 0.5;
-    double v_std = 0.5;
-    double v_max = 1.0;
     casadi::Opti ocp;
+    double T;
+    int N;
+    if(final){
+        T = std::max(1.5*Tf, 2.0);
+        N = std::max(estimate_N(T,L, v_max), 3.0);
+    } else{
+        T = T1;
+        N = N_init;
+    }
     Slice all;
-    double T = estimate_T1(3*N, L, v_max);
 
     auto x = std::make_shared<se1>();
     x->X =  ocp.variable(3, N+1) ;
@@ -104,51 +121,67 @@ std::vector<double> &xf_new, std::vector<double> &u0_new ){
     double T2 = (Tf-T);
     int N2 = 5;
 
-    auto xend = ocp.variable(3,N2+1);
-    auto uend = ocp.variable(2,N2);
-    for(int k = 0; k < N2; ++k) {
-        ocp.subject_to(uend(all, k) <= u_bound);
-        ocp.subject_to(uend(all, k) >= -u_bound);
-    }
-    for(int k = 0; k < N2; ++k) {
-        ocp.subject_to(xend(all, k) >= 0);
-    }
+    MX xend, uend, x_plot1;
+    x_plot1 = lgl_ms.getStates();
+    if(!final) { // patching polygon
+        xend = ocp.variable(3, N2 + 1);
+        uend = ocp.variable(2, N2);
+        for (int k = 0; k < N2; ++k) {
+            ocp.subject_to(uend(all, k) <= u_bound);
+            ocp.subject_to(uend(all, k) >= -u_bound);
+        }
+        for (int k = 0; k < N2; ++k) {
+            ocp.subject_to(xend(all, k) >= 0);
+        }
 
-    SX l2 = (u->v_ode-v_std)*(u->v_ode-v_std) + u->w_ode*u->w_ode;
+        SX l2 = (u->v_ode - v_std) * (u->v_ode - v_std) + u->w_ode * u->w_ode;
 /*    SX l2 = (x->x_ode-xf(0).scalar())*(x->x_ode-xf(0).scalar())
             + (x->y_ode-xf(1).scalar())*(x->y_ode-xf(1).scalar());*/
-    auto J2 = Function("l", {x->X_ode(), u->U_ode()}, {l2});
-    LGLms lgl_ms2 = LGLms(xend, uend, N2, T2, n2, f, J2, ocp);
-    MX cost2 = lgl_ms2.integrated_cost(0, T2, N2);
-    for(auto g_i : lgl_ms2.g){
-        ocp.subject_to(g_i == 0 );
+        auto J2 = Function("l", {x->X_ode(), u->U_ode()}, {l2});
+        LGLms lgl_ms2 = LGLms(xend, uend, N2, T2, n2, f, J2, ocp);
+        MX cost2 = lgl_ms2.integrated_cost(0, T2, N2);
+        for (auto g_i: lgl_ms2.g) {
+            ocp.subject_to(g_i == 0);
+        }
+
+        // Patch condition
+        ocp.subject_to(xend(all, 0) - x->X(all, N) == 0);
+        // End goal
+        //ocp.subject_to( xend(all, N2) - xf == 0 );
+
+        auto x_plot2 = lgl_ms2.getStates();
+        auto x_plot = MX::horzcat({x_plot1, x_plot2});
+
+        for (int k = 0; k < x->X.size2() - 1; k++) {
+            //cost = cost + mtimes(transpose(u->U(all, k + 1) - u->U(all, k)), u->U(all, k + 1) - u->U(all, k));
+            //cost = cost + sum1(sum2(u->U(all, k+1) -  u->U(all, k)));
+            //cost = cost + mtimes(transpose(x->X(all, k ) - xf),(x->X(all, k ) - xf));
+        }
+        cost = cost + mtimes(transpose(x->X(all, x->X.size2()-1) - xf), (x->X(all, x->X.size2()-1) - xf));
+        for (int k = 0; k < xend.size2() - 1; k++) {
+            //cost2 = cost2 + mtimes(transpose(uend(all, k + 1) - uend(all, k)), uend(all, k + 1) - uend(all, k));
+            //cost2 = cost2 + sum1(sum2(uend(all, k+1) -  uend(all, k)));
+            cost2 = cost2 + mtimes(transpose(xend(all, k) - xf), (xend(all, k) - xf));
+        }
+        auto slicexy = Slice(0, 2);
+        cost2 = cost2 + mtimes(transpose(xend(all, xend.size2()-1) - xf), (xend(all, xend.size2()-1) - xf));
+        //ocp.minimize((T*T)*cost+(T2*T2)*cost2);
+        ocp.minimize((T) * cost + (T2) * cost2);
+    } else{ // Final Trajectory
+        //ocp.subject_to(x->X(0, x->X.size2()-1) - xf[0]== 0);
+        //ocp.subject_to(x->X(2, x->X.size2()-1) - xf[2] <=  0.1);
+        //ocp.subject_to(x->X(2, x->X.size2()-1) - xf[2] >=  0.1);
+        //ocp.subject_to(x->X(1, x->X.size2()-1) - xf[1] <=  0.1);
+        //ocp.subject_to(x->X(1, x->X.size2()-1) - xf[1] >=  0.1);
+        ocp.subject_to(mtimes(transpose(x->X(all, x->X.size2()-1) - xf), (x->X(all, x->X.size2()-1) - xf)) <= 0.1);
+        for (int k = 0; k < x->X.size2() - 1; k++) {
+            //cost = cost + mtimes(transpose(u->U(all, k + 1) - u->U(all, k)), u->U(all, k + 1) - u->U(all, k));
+            //cost = cost + sum1(sum2(u->U(all, k+1) -  u->U(all, k)));
+            //cost = cost + mtimes(transpose(x->X(all, k ) - xf),(x->X(all, k ) - xf));
+        }
+        cost = cost + mtimes(transpose(x->X(all, x->X.size2()-1) - xf), (x->X(all, x->X.size2()-1) - xf));
+        ocp.minimize(cost );
     }
-
-    // Patch condition
-    ocp.subject_to(xend(all, 0) - x->X(all,N) ==0);
-    // End goal
-    //ocp.subject_to( xend(all, N2) - xf == 0 );
-
-    auto x_plot1 = lgl_ms.getStates();
-    auto x_plot2 = lgl_ms2.getStates();
-    auto x_plot = MX::horzcat({x_plot1, x_plot2});
-
-    for(int k = 0; k < N-1; k++){
-        cost = cost + mtimes(transpose(u->U(all, k+1) -  u->U(all, k)), u->U(all, k+1) -  u->U(all, k)) ;
-        //cost = cost + sum1(sum2(u->U(all, k+1) -  u->U(all, k)));
-        //cost = cost + mtimes(transpose(x->X(all, k ) - xf),(x->X(all, k ) - xf));
-    }
-    cost = cost + (1/10)*mtimes(transpose(x->X(all, N ) - xf),(x->X(all, N ) - xf));
-    for(int k = 0; k < N2-1; k++){
-        cost2 = cost2 + mtimes(transpose(uend(all, k+1) -  uend(all, k)), uend(all, k+1) -  uend(all, k)) ;
-        //cost2 = cost2 + sum1(sum2(uend(all, k+1) -  uend(all, k)));
-        cost2 = cost2 + mtimes(transpose(xend(all, k ) - xf),(xend(all, k ) - xf));
-    }
-    auto slicexy = Slice(0,2);
-    cost2 = cost2 + mtimes(transpose(xend(all, N2 ) - xf),(xend(all, N2 ) - xf));
-    //ocp.minimize((T*T)*cost+(T2*T2)*cost2);
-    ocp.minimize((T)*cost+(T2)*cost2);
-
 
 
     // 3 - Solve
@@ -166,15 +199,14 @@ std::vector<double> &xf_new, std::vector<double> &u0_new ){
     u0_new.push_back(Usol(0, Nu-1).scalar());
     u0_new.push_back(Usol(1, Nu-1).scalar());
 
-    x->X = MX::horzcat({x->X, xend});
-    u->U = MX::horzcat({u->U, uend});
+    if(!final){
+        x->X = MX::horzcat({x->X, xend});
+        u->U = MX::horzcat({u->U, uend});
+    };
 
     Plotter_dt plotter;
     DM Xsol = solution.value(x->X);
     Usol = solution.value(u->U);
-    std::cout << Xsol << std::endl;
-    std::cout << Usol << std::endl;
-    std::cout << solution.value(cost) << std::endl;
 
     // 4 - Plot Solution
     plotter.plot_path(Xsol(0,all), Xsol(1,all));
@@ -198,17 +230,25 @@ int main() {
 
     // Iter 1
     std::vector<double> xf_new, uf_new;
-    auto d = estimate_distance(x0.get_elements(), xf.get_elements());
-    rh(x0.get_elements(), {0.0,0.0},xf.get_elements(), estimate_T(d,v_std), xf_new, uf_new );
+    auto d1 = estimate_distance(x0.get_elements(), xf.get_elements());
+    auto T_total_1 = estimate_T(d1, v_std);
+    rh(x0.get_elements(), {0.0,0.0},xf.get_elements(), T_total_1, xf_new, uf_new );
 
     // Iter 2
     std::vector<double> xf_new2, uf_new2;
-    d = estimate_distance(xf_new, xf.get_elements());
-    rh(xf_new, uf_new,xf.get_elements(), estimate_T(d, v_std), xf_new2, uf_new2 );
+    auto d2 = estimate_distance(xf_new, xf.get_elements());
+    auto v_1 = d1/T_total_1;
+    auto v_2 = (d1 - d2)/T1;
+    auto v_avg = 0.5*v_1 + 0.5*v_2;
+    auto T_total_2 = d2 / v_avg;
+    rh(xf_new, uf_new,xf.get_elements(), T_total_2, xf_new2, uf_new2 );
 
     // Iter 3
     std::vector<double> xf_new3, uf_new3;
-    d = estimate_distance(xf_new2, xf.get_elements());
-    rh(xf_new2, uf_new2,xf.get_elements(), estimate_T(d, v_std), xf_new2, uf_new2 );
+    auto d3  = estimate_distance(xf_new2, xf.get_elements());
+    auto v_3 = (d2 - d3)/T1;
+    v_avg = 0.5*v_2 + 0.5*v_3;
+    auto T_total_3 = d3 / v_avg;
+    rh(xf_new2, uf_new2,xf.get_elements(), T_total_3, xf_new3, uf_new3 , true );
     return 0;
 }
